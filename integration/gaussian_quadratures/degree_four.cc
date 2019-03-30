f45df6ec68aa9f69e81465aceb3870a26954d56b
Lucas Primo <lucasprimo375@hotmail.com>
2019-03-30 16:40:48 -0300

    fixing gauss-legendre degree 4

--- integration/gaussian_quadratures/degree_four.cc
+++ integration/gaussian_quadratures/degree_four.cc
@@ -7,14 +7,14 @@ DegreeFour::DegreeFour(double a, double b) : GaussQuadrature(a, b) {
     degree = 4;
 
     roots = new double[degree];
-    roots[0] = -std::sqrt(std::sqrt(static_cast<double>(525) - 70*std::sqrt(30)))/35;
-    roots[1] = std::sqrt(std::sqrt(static_cast<double>(525) - 70*std::sqrt(30)))/35;
-    roots[2] = -std::sqrt(std::sqrt(static_cast<double>(525) + 70*std::sqrt(30)))/35;
-    roots[3] = std::sqrt(std::sqrt(static_cast<double>(525) + 70*std::sqrt(30)))/35;
+    roots[0] = static_cast<double>(std::sqrt((3 - 2*std::sqrt(6/5))/7));
+    roots[1] = -static_cast<double>(std::sqrt((3 - 2*std::sqrt(6/5))/7));
+    roots[2] = static_cast<double>(std::sqrt((3 + 2*std::sqrt(6/5))/7));
+    roots[3] = -static_cast<double>(std::sqrt((3 + 2*std::sqrt(6/5))/7));
 
     weights = new double[degree];
-    weights[0] = (static_cast<double>(18) + std::sqrt(30))/36;
-    weights[1] = (static_cast<double>(18) + std::sqrt(30))/36;
-    weights[2] = (static_cast<double>(18) - std::sqrt(30))/36;
-    weights[3] = (static_cast<double>(18) - std::sqrt(30))/36;
+    weights[0] = static_cast<double>((18 + std::sqrt(30))/36);
+    weights[1] = static_cast<double>((18 + std::sqrt(30))/36);
+    weights[2] = static_cast<double>((18 - std::sqrt(30))/36);
+    weights[3] = static_cast<double>((18 - std::sqrt(30))/36);
 }
